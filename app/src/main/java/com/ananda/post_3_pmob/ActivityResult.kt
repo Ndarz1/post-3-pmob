74978338c0b0c5762542733f69adcf4111a48531
Ndarz1 <anandarizn@gmail.com>
2025-10-20 22:58:56 +0700

    initial commit

--- app/src/main/java/com/ananda/post_3_pmob/ActivityResult.kt
+++ app/src/main/java/com/ananda/post_3_pmob/ActivityResult.kt
@@ -1,5 +1,6 @@
 package com.ananda.post_3_pmob
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,7 @@ class ActivityResult : AppCompatActivity() {
   
   private  lateinit var binding: ActivityResultBinding
   
+  @SuppressLint("SetTextI18n")
   override fun onCreate(savedInstanceState: Bundle?) {
     
     super.onCreate(savedInstanceState)
@@ -19,6 +21,19 @@ class ActivityResult : AppCompatActivity() {
     binding = ActivityResultBinding.inflate(layoutInflater)
     
     setContentView(binding.root)
+    
+    val fullName = intent.getStringExtra("fullName")
+    val userName = intent.getStringExtra("userName")
+    val age = intent.getStringExtra("age")
+    val email = intent.getStringExtra("email")
+    val gender = intent.getStringExtra("gender")
+    
+    binding.fullNameResult.text = "Full Name: $fullName"
+    binding.usernameResult.text = "Username: $userName"
+    binding.ageResult.text = "Age: $age"
+    binding.emailResult.text = "Email: $email"
+    binding.genderResult.text = "Gender: $gender"
+    
     
   }
 }
--- app/src/main/java/com/ananda/post_3_pmob/MainActivity.kt
+++ app/src/main/java/com/ananda/post_3_pmob/MainActivity.kt
@@ -2,29 +2,57 @@ package com.ananda.post_3_pmob
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.RadioButton
+import  android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.ananda.post_3_pmob.ActivityResult.Companion.DATA_ACC
-import com.ananda.post_3_pmob.ActivityResult.Companion.DATA_KELAMIN
-import com.ananda.post_3_pmob.ActivityResult.Companion.DATA_NAMA
 import com.ananda.post_3_pmob.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
   
   private lateinit var binding: ActivityMainBinding
-  private var jenisKelamin = ""
-  private var accBtn = true
   
   override fun onCreate(savedInstanceState: Bundle?) {
-    
-    
-    
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
-    
     setContentView(binding.root)
+    
+    binding.btnSubmit.setOnClickListener {
+      val fullName = binding.fullName.text.toString().trim()
+      val userName = binding.username.text.toString().trim()
+      val age = binding.age.text.toString().trim()
+      val email = binding.email.text.toString().trim()
+      val password = binding.password.text.toString()
+      val confirmPassword = binding.confirmPassword.text.toString()
+      
+      val selectedId = binding.gender.checkedRadioButtonId
+      val gender = if (selectedId != -1){
+        findViewById<RadioButton>(selectedId).text.toString()
+      } else {
+        ""
+      }
+      
+      if(fullName.isEmpty() || userName.isEmpty() || age.isEmpty()||email.isEmpty()||password.isEmpty()||confirmPassword.isEmpty()|| gender
+          .isEmpty()) {
+        Toast.makeText(this, "Semua kolom harus diisi", Toast.LENGTH_SHORT).show()
+        return@setOnClickListener
+      }
+      
+      if(password != confirmPassword){
+        Toast.makeText(this, "Password tidak sama", Toast.LENGTH_SHORT).show()
+        return@setOnClickListener
+      }
+      
+      val  intent = Intent(this, ActivityResult::class.java).apply {
+        putExtra("fullName", fullName)
+        putExtra("userName", userName)
+        putExtra("age", age)
+        putExtra("email", email)
+        putExtra("gender", gender)
+      }
+      
+      startActivity(intent)
+    }
     
  
     
