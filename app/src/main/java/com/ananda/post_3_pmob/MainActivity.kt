307b3501bf29ffeb166a16bb180ea5a68fc732e7
Ndarz1 <anandarizn@gmail.com>
2025-10-20 20:16:38 +0700

    initial commit

--- app/src/main/java/com/ananda/post_3_pmob/MainActivity.kt
+++ app/src/main/java/com/ananda/post_3_pmob/MainActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ananda.post_3_pmob.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  
+  private lateinit var binding: ActivityMainBinding
+  private var jenisKelamin = ""
+  private var accBtn = true
+  
   override fun onCreate(savedInstanceState: Bundle?) {
+    
+    
+    
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContentView(R.layout.activity_main)
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-      insets
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    
+    setContentView(binding.root)
+    
+    binding.apply {
+      btnSubmit.setOnClickListener {
+        val name = isinama.text.toString()
+        if(rdb1.isChecked){
+          jenisKelamin = "Laki - laki"
+        } else if (rdb2.isChecked) {
+          jenisKelamin = "Perempuan"
+        }
+      }
     }
+    
   }
 }
