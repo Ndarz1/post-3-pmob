c83ebc31c3c102cc66a3c0019ceaa073b8c09d78
Ndarz1 <anandarizn@gmail.com>
2025-10-20 20:30:17 +0700

    initial commit

--- app/src/main/java/com/ananda/post_3_pmob/ActivityResult.kt
+++ app/src/main/java/com/ananda/post_3_pmob/ActivityResult.kt
@@ -1,21 +1,52 @@
 package com.ananda.post_3_pmob
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ananda.post_3_pmob.databinding.ActivityResultBinding
 
 class ActivityResult : AppCompatActivity() {
+  
+  private  lateinit var binding: ActivityResultBinding
+  
   override fun onCreate(savedInstanceState: Bundle?) {
     
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContentView(R.layout.activity_result)
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-      insets
+    
+    binding = ActivityResultBinding.inflate(layoutInflater)
+    
+    setContentView(binding.root)
+    
+    val name = intent.getStringExtra(DATA_NAMA)
+    val jenisKelamin = intent.getStringExtra(DATA_KELAMIN)
+    val resultAcc = intent.getBooleanExtra(DATA_ACC, false)
+    
+    binding.apply {
+      tampilNama.text = name
+      tampilJenisKelamin.text = jenisKelamin
+      if (jenisKelamin == "Laki - laki"){
+        tampilJenisKelamin.setBackgroundColor(Color.GRAY)
+      }
+      if (jenisKelamin == "Perempuan") {
+        tampilJenisKelamin.setBackgroundColor(Color.MAGENTA)
+      }
+      if (resultAcc == true) {
+        tampilSetuju.setText("Data yang diisikan benar")
+        tampilSetuju.setBackgroundColor(Color.GREEN)
+      } else {
+        tampilSetuju.setText("Ada yang salah")
+        tampilSetuju.setBackgroundColor(Color.RED)
+      }
     }
+    
+  }
+  
+  companion object {
+    const val DATA_NAMA = "data_nama"
+    const val DATA_KELAMIN = "data_kelamin"
+    const val DATA_ACC = "data_acc"
   }
 }
--- app/src/main/java/com/ananda/post_3_pmob/MainActivity.kt
+++ app/src/main/java/com/ananda/post_3_pmob/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ananda.post_3_pmob
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -30,6 +31,9 @@ class MainActivity : AppCompatActivity() {
         } else if (rdb2.isChecked) {
           jenisKelamin = "Perempuan"
         }
+        
+        accBtn = checkAgree.isChecked()
+        
       }
     }
     
